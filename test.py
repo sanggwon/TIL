d28936f4544d4f03a0ec28bc92ca5dfae351da6f
sanggwon <ck2570@naver.com>
2019-01-04 17:22:56 +0900

    D3 | 190104 | CLI

--- test.py
+++ test.py
@@ -1,16 +1,21 @@
-n = list(range(1,6))
+# n = list(range(1,5))
 
-## Q1
+
+# ## Q1
 # n.reverse()
-# for i in n :
-#     a = "*"*(int(i))
-#     print(a)
+# # for i in n :
+# #     a = "*"*i
+# #     print(a)
 
-## Q2
-# for i in n :
-#     a = "*"*i
-#     print(a)
+# ## Q2
+# # for i in n :
+# #     a = "*"*i
+# #     print(a)
 
 ## Q3
-for i in n :
-    
+for i in range(1,6):
+    for j in range(1,6-i):
+        print(" ", end = "")
+    for j in range(1,i+1):
+        print("$",end="")
+    print()        
