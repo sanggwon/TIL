6801898545678b85ec00f6da5d35c8e6aabf9d22
sanggwon <ck2570@naver.com>
2019-01-04 10:43:48 +0900

    D03 | 190104 | CLI

---
+++ test.py
@@ -0,0 +1,16 @@
+n = list(range(1,6))
+
+## Q1
+# n.reverse()
+# for i in n :
+#     a = "*"*(int(i))
+#     print(a)
+
+## Q2
+# for i in n :
+#     a = "*"*i
+#     print(a)
+
+## Q3
+for i in n :
+    
